d30f12584b4210e9cad86b6a4decb2ded456aad8
Alex White <alex.white@hey.com>
2024-01-26 10:30:27 -0500

    Adding link for making a new post

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -70,7 +70,24 @@ export default function Page({ params }: { params: { username: string } }) {
         <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl w-full px-3">
           <main className="flex-1 flex flex-col gap-6">
             {userProfile?.id == user?.id ? (
-              <h1 className="text-2xl">your posts.</h1>
+              <div className="w-full flex flex-col sm:flex-row sm:items-center justify-between">
+                <h1 className="text-2xl">your posts.</h1>
+                <Link
+                  href="/"
+                  className="mt-1 sm:mt-0 text-base text-gray-700 dark:text-white font-medium flex flex-row items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5 mr-1"
+                  >
+                    <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+                    <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                  </svg>
+                  make a new post
+                </Link>
+              </div>
             ) : (
               <h1 className="text-2xl">{userProfile.full_name}'s posts.</h1>
             )}
